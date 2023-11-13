084d8e539131d38663968937dbe6502a0b220898
RiccardoSansone <r.sansone@gmail.com>
2023-11-13 17:13:08 +0100

    recovered file from wrong repo

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
@@ -16,5 +17,9 @@ class Project extends Model
         'projectlink',
         'projectgithub'
     ];
+
+    public function type(): BelongsTo {
+        return $this->belongsTo(Type::class);
+    }
 }
 
